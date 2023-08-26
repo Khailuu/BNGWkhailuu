68c11c51c4c94e7eb368c61ad679ab9c0aecb8ff
khailuu <khailuu123@gmail.com>
2023-08-26 11:18:42 +0700

    assignment

--- assets/home.js
+++ assets/home.js
@@ -3,7 +3,26 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const btnClose = document.querySelector('.icon-close');
+const btnMenu = document.getElementById('btn-menu');
+const popUpMenu = document.querySelector('.popup-menu');
+const closeMenu = document.querySelector('.active-menu');
+if (btnMenu) {
+    btnMenu.addEventListener('click', ()=> {
+        popUpMenu.classList.add('active-menu');
+    })
+}
 
+// if(btnMenu) {
+//     btnMenu.addEventListener('click', ()=> {
+//         popUpMenu.classList.remove('active-menu');
+//     })
+// }
+
+if (btnPopup) {
+    btnPopup.addEventListener('click', ()=> {
+        popUpMenu.classList.remove('active-menu');
+    })
+}
 
 if(btnPopup){
     btnPopup.addEventListener('click', ()=> {
@@ -17,17 +36,15 @@ if(btnClose) {
         wrapper.classList.remove('active-popup');
        
     })
-    btnClose.addEventListener('click', ()=> {
-        wrapper.classList.remove('ative-popup active');
-    })
+ 
 }
-
 
 if(registerLink){
     registerLink.addEventListener('click', ()=> {
         wrapper.classList.add('active');
     })
 }
+
 if(loginLink){
     loginLink.addEventListener('click', ()=> {
         wrapper.classList.remove('active');
